0d0dace9f094d3e60e19854a9e5056298ab43593
Rechra001 <tkropf12345@gmail.com>
2023-09-07 11:34:10 +0200

    Updated

--- KW Sales App/KW Sales UI/ViewModels/MainViewModel.cs
+++ KW Sales App/KW Sales UI/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KW_Sales_UI.Commands;
+using KW_Sales_UI.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,37 @@ using System.Threading.Tasks;
 
 namespace KW_Sales_UI.ViewModels
 {
-    class MainViewModel : BaseViewModel
+    public class MainViewModel : BaseViewModel
     {
+		private INavigationService _navigation;
+
+		public INavigationService Navigation
+		{
+			get { return _navigation; }
+			set 
+			{ 
+				_navigation = value;
+				OnPropertyChanged("Navigation");
+			}
+		}
+
+        public RelayCommand NavigateToOrderViewCommand { get; set; }
+
+        public MainViewModel(INavigationService navigation)
+        {
+			Navigation = navigation;
+
+			NavigateToOrderViewCommand = new RelayCommand(NavigateToOrderView, CanNavigateToOrderView);
+        }
+
+        private bool CanNavigateToOrderView(object obj)
+        {
+            return true;
+        }
+
+        private void NavigateToOrderView(object obj)
+        {
+            Navigation.NavigateTo<OrderViewModel>();
+        }
     }
 }
