1fcb1ad40f13ed02781d7f070d8de1b6b412ac80
Rechra001 <tkropf12345@gmail.com>
2023-09-09 09:53:43 +0200

    Customer repository

---
+++ KW Sales App/DataAccessLibrary/Repositories/CustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private string? _connectionString;
+        public string ConnectionString { get => _connectionString!; set => _connectionString = value; }
+
+        public CustomerRepository(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public CustomerModel Insert(CustomerModel customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
