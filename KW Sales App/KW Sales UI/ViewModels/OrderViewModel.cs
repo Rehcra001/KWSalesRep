3f2f1b660c63592f98b8f5627aebe72da5e8a9a0
Rehcra001 <tkropf12345@gmail.com>
2023-09-09 17:36:25 +0200

    Added ability to add a new customer or cancel the add. Using ICommand for binding to view.

--- KW Sales App/KW Sales UI/ViewModels/OrderViewModel.cs
+++ KW Sales App/KW Sales UI/ViewModels/OrderViewModel.cs
@@ -8,6 +8,7 @@ using DataAccessLibrary.Repositories;
 using DataAccessLibrary.Models;
 using System.CodeDom.Compiler;
 using System.Windows;
+using KW_Sales_UI.Commands;
 
 namespace KW_Sales_UI.ViewModels
 {
@@ -15,6 +16,7 @@ namespace KW_Sales_UI.ViewModels
     {
 		#region properties
 		private string _errorMessage;
+		private string _state;
 
         private ICustomerRepository? _customerRepository;
 
@@ -87,15 +89,37 @@ namespace KW_Sales_UI.ViewModels
 			}
 		}
 
-		#endregion properties
+		private bool _canAddCustomer;
+
+		public bool CanAddCustomer
+		{
+			get { return _canAddCustomer; }
+			set 
+			{ 
+				_canAddCustomer = value;
+				OnPropertyChanged("CanAddCustomer");
+			}
+		}
 
 
-		public OrderViewModel(INavigationService navigation, 
+		public RelayCommand AddNewCustomerCommand { get; set; }
+		public RelayCommand SaveNewCustomerCommand { get; set; }
+
+        public RelayCommand CancelNewCustomerCommand { get; set; }
+
+        #endregion properties
+
+
+        public OrderViewModel(INavigationService navigation, 
 							  IDataConnectionStringService dataConnection)
         {
 			Navigation = navigation;
 			DataConnection = dataConnection;
 			CustomerRepository = new CustomerRepository(DataConnection.SqlConnectionString);
+
+			AddNewCustomerCommand = new RelayCommand(AddNewCustomer, CanAddNewCustomer);
+			SaveNewCustomerCommand = new RelayCommand(SaveNewCustomer, CanSaveNewCustomer);
+			CancelNewCustomerCommand = new RelayCommand(CancelNewCustomer, CanCancelNewCustomer);
 
 			//Load all Customers
             Tuple<IEnumerable<CustomerModel>, string> tuple = CustomerRepository.GetAll().ToTuple();
@@ -110,6 +134,61 @@ namespace KW_Sales_UI.ViewModels
 								"ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
 				return;
 			}
+			SetState("View");
         }
+
+        private bool CanCancelNewCustomer(object obj)
+        {
+			return !_state.Equals("View");
+        }
+
+        private void CancelNewCustomer(object obj)
+        {
+            if (_state.Equals("Add"))
+			{
+				//Delete the last Record
+				int index = Customers.Count - 1;
+				Customers.RemoveAt(index);
+				SetState("View");
+				CustomersIndex = 0;
+			}
+        }
+
+        private bool CanSaveNewCustomer(object obj)
+        {
+            return !_state.Equals("View");
+        }
+
+        private void SaveNewCustomer(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanAddNewCustomer(object obj)
+        {
+			return _state.Equals("View");
+        }
+
+        private void AddNewCustomer(object obj)
+        {			
+			Customers.Add(new CustomerModel());
+			CustomersIndex = Customers.Count - 1;
+            SetState("Add");
+        }
+
+		private void SetState(string state)
+		{
+			_state = state;
+
+			switch (_state)
+			{
+				case "View":
+                    CanAddCustomer = true;
+					break;
+				default:
+                    CanAddCustomer = false;
+					break;
+			}
+		}
     }
 }
