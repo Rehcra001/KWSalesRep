29e1fad9ac8232bf7730ce02b5d7b35811d844c1
Rechra001 <tkropf12345@gmail.com>
2023-09-09 09:53:10 +0200

    Added customer repository that will handle all the customer CRUD operations with the datasource

---
+++ KW Sales App/DataAccessLibrary/Models/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public interface ICustomerRepository
+    {
+        public string ConnectionString { get; set; }
+        CustomerModel Insert(CustomerModel customer);
+
+    }
+}
